0157ce55823048ef98e17c64ef866bc76946caba
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-08-01 13:29:48 -0500

    added in ConfigService to manage services for AppListener

---
+++ src/main/java/com/hervey/app/services/ConfigService.java
@@ -0,0 +1,31 @@
+package com.hervey.app.services;
+
+import org.springframework.stereotype.Service;
+
+import com.hervey.app.models.Role;
+import com.hervey.app.repositories.RoleRepository;
+
+@Service
+public class ConfigService {
+	
+	private final RoleRepository roleRepository;
+	
+	public ConfigService(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
+
+	public Long fetchCountRoles() {
+
+		Long rolesCount = roleRepository.count();
+		return rolesCount;
+	}
+	
+	
+	public void createRole(Role role) {
+		roleRepository.save(role);
+	}
+	//Sample
+	
+
+
+}
