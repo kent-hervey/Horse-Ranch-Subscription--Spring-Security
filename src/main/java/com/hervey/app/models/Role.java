79a6a07d95d1295aa7be12839c3395debbf793e6
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-08 17:33:20 -0500

    added role explanation field

--- src/main/java/com/hervey/app/models/Role.java
+++ src/main/java/com/hervey/app/models/Role.java
@@ -31,6 +31,8 @@ public class Role {
 	private Date createdAt;
 
 	private Date updateAt;
+	
+	String roleExplanation;
 
 	//@ManyToMany(mappedBy ="roles")
 	@ManyToMany(fetch = FetchType.EAGER, mappedBy="roles")
@@ -92,6 +94,17 @@ public class Role {
 		this.users = users;
 	}
 
+	
+	
+	
+	public String getRoleExplanation() {
+		return roleExplanation;
+	}
+
+	public void setRoleExplanation(String roleExplanation) {
+		this.roleExplanation = roleExplanation;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -99,6 +112,7 @@ public class Role {
 		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((roleExplanation == null) ? 0 : roleExplanation.hashCode());
 		result = prime * result + ((updateAt == null) ? 0 : updateAt.hashCode());
 		result = prime * result + ((users == null) ? 0 : users.hashCode());
 		return result;
@@ -128,6 +142,11 @@ public class Role {
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (roleExplanation == null) {
+			if (other.roleExplanation != null)
+				return false;
+		} else if (!roleExplanation.equals(other.roleExplanation))
+			return false;
 		if (updateAt == null) {
 			if (other.updateAt != null)
 				return false;
@@ -143,7 +162,8 @@ public class Role {
 
 	@Override
 	public String toString() {
-		return "Role [id=" + id + ", name=" + name + ", createdAt=" + createdAt + ", updateAt=" + updateAt + "]";
+		return "Role [id=" + id + ", name=" + name + ", createdAt=" + createdAt + ", updateAt=" + updateAt
+				+ ", roleExplanation=" + roleExplanation + "]";
 	}
 
 }
