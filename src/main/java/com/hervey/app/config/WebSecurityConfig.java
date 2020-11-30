22483a76bad58b377ea39a36082350ee3861ab92
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-11-30 17:10:42 -0600

    added "/entry" to configure...http.authorize

--- src/main/java/com/hervey/app/config/WebSecurityConfig.java
+++ src/main/java/com/hervey/app/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		
 		
-			.antMatchers("/css/**", "/images/**", "/js/**", "/registration", "/loginreg", "/ranches/entry").permitAll() 
+			.antMatchers("/css/**", "/images/**", "/js/**", "/registration", "/loginreg", "/ranches/entry", "/entry").permitAll() 
 			
 			//below turns off all security for trouble shooting
 			//.antMatchers("/**").permitAll()
