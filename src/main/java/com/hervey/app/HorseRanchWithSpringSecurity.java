1199e678945ff7cc1e918c256655604cacac192f
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-12-05 17:11:37 -0600

    HorseRanchWithSecurity.java reverting to old way ajp

--- src/main/java/com/hervey/app/HorseRanchWithSpringSecurity.java
+++ src/main/java/com/hervey/app/HorseRanchWithSpringSecurity.java
@@ -14,20 +14,34 @@ public class HorseRanchWithSpringSecurity {
 		SpringApplication.run(HorseRanchWithSpringSecurity.class, args);
 	}
 
-	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		Connector ajpConnector = new Connector("AJP/1.3");
-		ajpConnector.setPort(9090);
-		ajpConnector.setSecure(false);
-		ajpConnector.setAllowTrace(false);
-		ajpConnector.setScheme("http");
-		
-		final AbstractAjpProtocol protocol = (AbstractAjpProtocol) ajpConnector.getProtocolHandler();
-		ajpConnector.setSecure(true);
-		protocol.setSecret("myAJPSecret-you-cant-guess-this");
-		tomcat.addAdditionalTomcatConnectors(ajpConnector);
-		return tomcat;
-	}
+//	@Bean
+//	public TomcatServletWebServerFactory servletContainer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//		Connector ajpConnector = new Connector("AJP/1.3");
+//		ajpConnector.setPort(9090);
+//		ajpConnector.setSecure(false);
+//		ajpConnector.setAllowTrace(false);
+//		ajpConnector.setScheme("http");
+//		
+//		final AbstractAjpProtocol protocol = (AbstractAjpProtocol) ajpConnector.getProtocolHandler();
+//		ajpConnector.setSecure(true);
+//		protocol.setSecret("myAJPSecret-you-cant-guess-this");
+//		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//		return tomcat;
+//	}
 
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+    }
+	
+	
+	
 }
