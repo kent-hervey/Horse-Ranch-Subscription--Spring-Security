fd23a12aa1d2c809442699ee5b4da596d7fbfef6
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-12-06 16:56:31 -0600

    commented out ajp code for use with AWS EB

--- src/main/java/com/hervey/app/HorseRanchWithSpringSecurity.java
+++ src/main/java/com/hervey/app/HorseRanchWithSpringSecurity.java
@@ -30,18 +30,18 @@ public class HorseRanchWithSpringSecurity {
 //		return tomcat;
 //	}
 
-    @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.setPort(9090);
-        ajpConnector.setSecure(false);
-        ajpConnector.setAllowTrace(false);
-        ajpConnector.setScheme("http");
-        tomcat.addAdditionalTomcatConnectors(ajpConnector);
-        return tomcat;
-    }
-	
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        Connector ajpConnector = new Connector("AJP/1.3");
+//        ajpConnector.setPort(9090);
+//        ajpConnector.setSecure(false);
+//        ajpConnector.setAllowTrace(false);
+//        ajpConnector.setScheme("http");
+//        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//        return tomcat;
+//    }
+//	
 	
 	
 }
